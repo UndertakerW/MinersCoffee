576e4c763c55c1c762fc6e99170042b3190259f1
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2021-05-01 23:40:29 +0800

    Update Test Framework

--- Code/References/MinerLamp-master/MinerLamp-master/tst_generaltest.cpp
+++ Code/References/MinerLamp-master/MinerLamp-master/tst_generaltest.cpp
@@ -4,6 +4,7 @@
 #include <QFile>
 #include <QDebug>
 #include <qtest.h>
+#include <sstream>
 
 #include "nvidianvml.h"
 #include "jsonparser.h"
@@ -19,12 +20,15 @@ public:
 
 private:
     QList<QString> GetStringData(const QString& path);
+    void ShowDataError(const QString& filename1, const QString& filename2);
 
 private slots:
     void initTestCase();
     void cleanupTestCase();
     void test_ParseJsonForMining();
     void test_ParseJsonForMining_data();
+    void test_ParseJsonForPool();
+    void test_ParseJsonForPool_data();
 
 };
 
@@ -48,11 +52,65 @@ void GeneralTest::cleanupTestCase()
 
 }
 
+void GeneralTest::test_ParseJsonForPool()
+{
+    QFETCH(QString, input);
+    QFETCH(QString, currency);
+    QFETCH(float, income);
+    QFETCH(float, usd);
+
+    PoolJsonParser poolJsonParser;
+    QList<PoolInfo> poolInfos = poolJsonParser.ParseJsonForPool(input.toStdString());
+
+    QString out1;
+    float out2 = 0;
+    float out3 = 0;
+    if (poolInfos.size() > 0)
+    {
+        out1 = poolInfos[0].currency;
+        out2 = poolInfos[0].income;
+        out3 = poolInfos[0].usd;
+    }
+    QCOMPARE(out1, currency);
+    QCOMPARE(out2, income);
+    QCOMPARE(out3, usd);
+
+    QBENCHMARK {
+        QList<PoolInfo> poolInfos = poolJsonParser.ParseJsonForPool(input.toStdString());
+    }
+}
+
+void GeneralTest::test_ParseJsonForPool_data()
+{
+    QString input_path = qApp->applicationDirPath() + "/test/test_ParseJsonForPool_input.txt";
+    QList<QString> input = GetStringData(input_path);
+    QString result_path = qApp->applicationDirPath() + "/test/test_ParseJsonForPool_result.txt";
+    QList<QString> result = GetStringData(result_path);
+    QTest::addColumn<QString>("input");
+    QTest::addColumn<QString>("currency");
+    QTest::addColumn<float>("income");
+    QTest::addColumn<float>("usd");
+    for (int i = 0; i < input.size(); i++)
+    {
+        std::string rowName = QString("Case %1").arg(i).toStdString();
+        QStringList results = result[i].split(",");
+        if (results.size() != 3)
+        {
+            ShowDataError(input_path, result_path);
+            break;
+        }
+        QTest::newRow(rowName.c_str())
+                << input[i] << results[0]
+                << results[1].toFloat() << results[2].toFloat();
+    }
+}
+
+
 void GeneralTest::test_ParseJsonForMining()
 {
     QFETCH(QString, input);
     QFETCH(unsigned int, latency);
-    QFETCH(float, GPU0_hashrate);
+    QFETCH(float, gpu0_hashrate);
     NBMinerJsonParser nbMinerJsonParser;
     MiningInfo miningInfo = nbMinerJsonParser.ParseJsonForMining(input.toStdString());
     unsigned int out1 = miningInfo.latency;
@@ -62,7 +120,8 @@ void GeneralTest::test_ParseJsonForMining()
         out2 = miningInfo.gpuMiningInfos[0].hashrate;
     }
     QCOMPARE(out1, latency);
-    QCOMPARE(out2, GPU0_hashrate);
+    QCOMPARE(out2, gpu0_hashrate);
+
     QBENCHMARK {
         MiningInfo miningInfo = nbMinerJsonParser.ParseJsonForMining(input.toStdString());
     }
@@ -70,26 +129,32 @@ void GeneralTest::test_ParseJsonForMining()
 
 void GeneralTest::test_ParseJsonForMining_data()
 {
-    QString input_path = qApp->applicationDirPath() + "/test/MiningJson.txt";
+    QString input_path = qApp->applicationDirPath() + "/test/test_ParseJsonForMining_input.txt";
     QList<QString> input = GetStringData(input_path);
-    QString result_path = qApp->applicationDirPath() + "/test/MiningJsonResult.txt";
+    QString result_path = qApp->applicationDirPath() + "/test/test_ParseJsonForMining_result.txt";
     QList<QString> result = GetStringData(result_path);
-    if (input.size()*2 != result.size())
-    {
-        QMessageBox::warning(NULL, "warning",
-                             QString("Corrupted testing data in %1 and/or %2").arg(input_path).arg(result_path));
-        return;
-    }
+
     QTest::addColumn<QString>("input");
     QTest::addColumn<unsigned int>("latency");
-    QTest::addColumn<float>("GPU0_hashrate");
+    QTest::addColumn<float>("gpu0_hashrate");
     for (int i = 0; i < input.size(); i++)
     {
         std::string rowName = QString("Case %1").arg(i).toStdString();
+        QStringList results = result[i].split(",");
+        if (results.size() != 2)
+        {
+            ShowDataError(input_path, result_path);
+            break;
+        }
         QTest::newRow(rowName.c_str())
-                << input[i] << result[i*2].toUInt() << result[i*2+1].toFloat();
+                << input[i] << results[0].toUInt() << results[1].toFloat();
     }
+}
 
+void GeneralTest::ShowDataError(const QString& filename1, const QString& filename2)
+{
+    QMessageBox::warning(NULL, "warning",
+                         QString("Corrupted test data in\n %1 \nand/or\n %2").arg(filename1).arg(filename2));
 }
 
 QList<QString> GeneralTest::GetStringData(const QString& path)
