b81647aed4adfcc66f5ea3ea12ba30547e167bc3
MoyuST <46509809+MoyuST@users.noreply.github.com>
2021-05-03 16:20:12 +0800

    add fonts
    add fonts

--- Code/References/MinerLamp-master/MinerLamp-master/main.cpp
+++ Code/References/MinerLamp-master/MinerLamp-master/main.cpp
@@ -4,17 +4,12 @@
 #include <QtSql/QSqlDatabase>
 int main(int argc, char *argv[])
 {
-    qDebug() << "loading fonts";
-    QFontDatabase::addApplicationFont(":/fonts/BRLNSB.TTF");
-    qDebug() << "loading fonts 1";
-    QFontDatabase::addApplicationFont(":/fonts/BRLNSDB.TTF");
-    qDebug() << "loading fonts 2";
-    QFontDatabase::addApplicationFont(":/fonts/BRLNSR.TTF");
-    qDebug() << "finishing fonts";
-
     QApplication a(argc, argv);
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
     MainWindow w;
+    QFontDatabase::addApplicationFont(":/fonts/fonts/BRLNSB.TTF");
+    QFontDatabase::addApplicationFont(":/fonts/fonts/BRLNSDB.TTF");
+    QFontDatabase::addApplicationFont(":/fonts/fonts/BRLNSR.TTF");
     w.show();
     return a.exec();
 }
