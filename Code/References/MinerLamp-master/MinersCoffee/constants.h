c64bdd8eb30a6ee8047428c9e1c62b290b732776
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2021-05-11 17:04:16 +0800

    Update constants.h

--- Code/References/MinerLamp-master/MinersCoffee/constants.h
+++ Code/References/MinerLamp-master/MinersCoffee/constants.h
@@ -3,18 +3,25 @@
 
 #include <QString>
 
-static QString path_nbminer = "/cores/NBMiner_Win_37.1/nbminer.exe";
+// Files
 
 const QString pools_path = "data/pools.txt";
 
+const QString oc_advices_path = "data/advice.txt";
+
+static QString path_nbminer = "/cores/NBMiner_Win_37.1/nbminer.exe";
+
+const QString nvml_path = "C:\\Program Files\\NVIDIA Corporation\\NVSMI\\nvml.dll";
+
+// Commands
+
 static QString cmd_nbminer_eth = "-a ethash -o %1 -u %2.%3";
+
+// APIs
 
 const QString api_nbminer = "http://localhost:22333/api/v1/status";
 
 const QString sparkpool_api_str = "https://www.sparkpool.com/v1/pool/stats";
 
-const QString oc_advices_path = "data/advice.txt";
-
-const QString nvml_path = "C:\\Program Files\\NVIDIA Corporation\\NVSMI\\nvml.dll";
 
 #endif // CONSTANTS_H
